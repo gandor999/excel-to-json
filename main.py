ee219a80a99271582e750aa87db7b18e5bc10e97
Geodor Ruales <gandor999@gmail.com>
2023-07-26 17:34:55 +0800

    feat: initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import json
+
+
+def write_to_file(filename, data):
+
+
+
+def list_to_dict(list):
+
+
+
+def excel_to_json_by_sheet(filename, sheet_name):
+    # to get rid of null convert to json first then to dict
+
+
+
